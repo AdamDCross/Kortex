397cd533329040327ad2264a178589c39a2e3521
Phil-DS <phillipdesousa9@gmail.com>
2016-01-22 12:05:57 +0000

    Fixing uploading issue
    Readding DijkstraTest
    Removed team.jpg
    Started Tilemap.java

---
+++ src/dijkstra/DijkstraTest.java
@@ -0,0 +1,65 @@
+package dijkstra;
+
+import main.*;
+
+import main.Window;
+import org.jsfml.graphics.PrimitiveType;
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Vertex;
+import org.jsfml.system.*;
+
+/**
+ * Originally 'InterfaceTest' by Phil
+ * Tweaked by Vince on 14/01/2016 by separating window functionality from core functionality.
+ */
+public class DijkstraTest implements Render {
+    private Pathfind d;
+    private Vector2i currentPosition;
+    private RenderWindow window;
+
+    public DijkstraTest() {
+        d = new Pathfind(9,10);
+        currentPosition = new Vector2i(0,0);
+        this.window = Window.getInstance().getGameWindow();
+
+        for (int cy = 0; cy < Pathfind.GRID_HEIGHT; cy++) {
+            for(int cx = 0; cx< Pathfind.GRID_WIDTH; cx++) {
+                System.out.print(d.getCells()[cx][cy].weight+"\t");
+            }
+            System.out.println(" ");
+        }
+        System.out.println("\n------------------------------------------\n");
+
+        for (int cy = 0; cy < Pathfind.GRID_HEIGHT; cy++) {
+            for(int cx = 0; cx< Pathfind.GRID_WIDTH; cx++) {
+                System.out.print(d.getCells()[cx][cy].distance+"\t");
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public void updateCurrentMousePosition(Vector2i currentPosition) {
+        //fixes the mouse coords when the window is increased/decreased, and clamps them into the current view
+        Vector2f hold=Window.getInstance().getGameWindow().mapPixelToCoords(currentPosition);
+        this.currentPosition = new Vector2i((int)GameMaths.clamp(hold.x,0,Pathfind.GRID_WIDTH*Pathfind.GRID_SIZE),
+                (int) GameMaths.clamp(hold.y,0,Pathfind.GRID_HEIGHT*Pathfind.GRID_SIZE));
+    }
+
+    @Override
+    public void render() {
+        for(int i=0;i<Pathfind.GRID_WIDTH;i++){
+            for(int j=0;j<Pathfind.GRID_HEIGHT;j++){
+                d.getCells()[i][j].drawMe(window);
+            }
+        }
+        Cell start=d.getCells()[GameMaths.clamp(currentPosition.x/32,0,Pathfind.GRID_WIDTH-1)][GameMaths.clamp(currentPosition.y/32,0,Pathfind.GRID_HEIGHT-1)];
+        while(start.pathNext!=null){
+            window.draw(new Vertex[]{new Vertex(new Vector2f(start.pos.x*32+16,start.pos.y*32+16)),new Vertex(new Vector2f(start.pathNext.pos.x*32+16,start.pathNext.pos.y*32+16))}, PrimitiveType.LINE_STRIP);
+            start=start.pathNext;
+        }
+    }
+
+    @Override
+    public void update() {
+    }
+}
