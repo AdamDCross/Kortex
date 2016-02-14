98ff0f547a4130881539a143728437f1917768f1
vince0656 <vince0656@googlemail.com>
2016-02-14 08:40:41 +0000

    Turrets rotate!
    -Check out the ingame state for a rotating turret. in time, how smooth it rotates can be altered. Important to note that angle of rotation is in radians.
    -Getters and setters added to the turret class for later upgrading from the tech tree.

--- src/player/Turret.java
+++ src/player/Turret.java
@@ -17,7 +17,7 @@ public class Turret implements Render {
     private boolean visible;
     private boolean destroyed;
     private int health;
-    private int rotationSpeed;
+    private int rotationDelay;
     private float currentAngle;
     private float rotationAngle;
     private Button top;
@@ -26,22 +26,24 @@ public class Turret implements Render {
     private int xpRequirement;
     private int range;
     private int AOESize;
-    private boolean shieldActive;
+    private boolean shieldActive; //TODO: implement shield functionality
     private int shieldTimer;
     private int enemyKillCount;
     private String ID;
     private Animation explosion;
+    private long prevTime;
+    private long currentTime;
     private long localElapsedTime;
 
     // TODO: 08/02/2016 Shoot method.
 
-    public Turret(String top, String bottom, boolean visible, int health, float angle, float rotationAngle, int rotationSpeed, FloatRect dimensions,
+    public Turret(String top, String bottom, boolean visible, int health, float angle, float rotationAngle, int rotationDelay, FloatRect dimensions,
                    int scrapCost, int xpRequirement, int range, int AOESize, boolean shieldActive, int shieldTimer,
                    String ID, String explosion, int width, int height, int row, int col, int delay) {
         this.visible = visible;
         destroyed = false;
         this.health = health;
-        this.rotationSpeed = rotationSpeed;
+        this.rotationDelay = rotationDelay;
         this.currentAngle = angle;
         this.rotationAngle = rotationAngle;
 
@@ -65,24 +67,27 @@ public class Turret implements Render {
         this.ID = ID;
         localElapsedTime = 0;
         this.explosion = new Animation(explosion, width, height, row, col, delay, new Vector2f(dimensions.left, dimensions.top), 0, dimensions, false);
+        currentTime = 0;
+        prevTime = 0;
     }
     
     @Override
     public void update() {
-        long windowElapsedTime = Window.getInstance().getElapsedTime();
-        localElapsedTime += windowElapsedTime - localElapsedTime;
+        prevTime = currentTime;
+        currentTime = Window.getInstance().getElapsedTime();
+
+        localElapsedTime += currentTime - prevTime;
 
         if(destroyed){
             explosion.update();
         }
         else if(visible){
-            //rotate !!!!!!!!!!!!!!!!!!!!!!!!!
-            if( localElapsedTime >= rotationSpeed && localElapsedTime != windowElapsedTime ){
+            if( localElapsedTime >= rotationDelay ){
                 localElapsedTime = 0;
                 currentAngle += rotationAngle;
                 top.setAngleOfImage(currentAngle);
 
-                if(currentAngle >= 360.0f){
+                if(currentAngle >= (360.0f * 3.14 / 180)){
                     currentAngle = 0.0f;
                 }
             }
@@ -110,12 +115,56 @@ public class Turret implements Render {
         }
     }
 
+    public String getID(){
+        return ID;
+    }
+
+    public int getEnemyKillCount(){
+        return enemyKillCount;
+    }
+
+    public void setShieldTimer(int shieldTimer){
+        this.shieldTimer = shieldTimer;
+    }
+
+    public void setAOESize(int AOESize){
+        this.AOESize = AOESize;
+    }
+
+    public int getAOESize(){
+        return AOESize;
+    }
+
+    public void setRange(int range){
+        this.range = range;
+    }
+
+    public int getRange(){
+        return range;
+    }
+
+    public void setXPRequirement(int requirement){
+        xpRequirement = requirement;
+    }
+
+    public int getXPRequirement(){
+        return xpRequirement;
+    }
+
+    public void setScrapCost(int scrapCost){
+        this.scrapCost = scrapCost;
+    }
+
     public int getScrapCost(){
         return scrapCost;
     }
 
-    public String getID(){
-        return ID;
+    public void setRotationDelay(int delay){
+        rotationDelay = delay;
+    }
+
+    public int getTurretHealth(){
+        return health;
     }
 
     public void takeDamage(int damageCount){
--- src/states/Game.java
+++ src/states/Game.java
@@ -43,9 +43,9 @@ public class Game extends State {
         test = new DijkstraTest();
 
         red = new PatrollingEnemy(new Vector2f(0,50), new Vector2f(250, 50), 5);
-        //gameObjects.addElement(test);
-        //gameObjects.addElement(red);
-        //gameObjects.addElement(a);
+        gameObjects.addElement(test);
+        gameObjects.addElement(red);
+        gameObjects.addElement(a);
 
         setupGame();
         panel = new HUD(0.10f, 0.1f, this);
@@ -53,8 +53,8 @@ public class Game extends State {
 
         Vector<ArtAsset> turrets = AssetManager.getInstance().getArtAssetByAssetType("TURRET");
         Turret turretTest = new Turret(turrets.elementAt(0).getAssetPath(),
-                turrets.elementAt(1).getAssetPath(),true,100,0.0f,2.0f,50,
-                new FloatRect(panel.getGameWindowRect().left, panel.getGameWindowRect().top, 100.0f,100.0f),
+                turrets.elementAt(1).getAssetPath(),true,100,0.0f,2.0f,125,
+                new FloatRect(panel.getGameWindowRect().left,  panel.getGameWindowRect().height - 100, 100.0f,100.0f),
                 0,0,0,0,false,0,"TEST","src/assets/explosions/explosiontilesheet.png",128,140,10,10,50);
 
         gameObjects.addElement(turretTest);
