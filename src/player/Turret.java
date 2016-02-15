738a9063692d6b43c8aca4796c278d64e59c4b95
Phil-DS <phillipdesousa9@gmail.com>
2016-02-15 16:39:34 +0000

    Update Turret.java
    Updated manually from master to make it easy to merge old version and new version

--- src/player/Turret.java
+++ src/player/Turret.java
@@ -1,8 +1,11 @@
 package player;
 
-import enemy.Enemy;
 import graphics.Animation;
+import graphics.Image;
+import main.Button;
 import main.Render;
+import main.Window;
+import org.jsfml.graphics.FloatRect;
 import org.jsfml.system.Vector2f;
 
 import java.util.Vector;
@@ -14,116 +17,194 @@ public class Turret implements Render {
     private boolean visible;
     private boolean destroyed;
     private int health;
-    private float rotationSpeed;
-    private float angle;
-    private float speed;
-    private Animation top;
-    private Animation bottom;
-    private Vector2f position;
+    private int rotationDelay;
+    private float currentAngle;
+    private float rotationAngle;
+    private Button top;
+    private Button bottom;
+    private int scrapCost;
+    private int xpRequirement;
+    private int range;
+    private int AOESize;
+    private boolean shieldActive; //TODO: implement shield functionality
+    private int shieldTimer;
+    private int enemyKillCount;
+    private long XPForEnemyKills;
+    private int XPMultiplier;
+    private long attackScore;
+    private String ID;
+    private Animation explosion;
+    private long prevTime;
+    private long currentTime;
+    private long localElapsedTime;
 
-    // TODO: 08/02/2016 Shoot method
 
-    //variables for shooting
+    // TODO: 08/02/2016 Shoot method. Shoot method needs to take into account what enemy it's shooting etc. so it can calculate the correct XP and score gained etc
+    //TODO: Atack score is calculated from things like how long the turret has been hitting the enemy for etc.
 
-    private long shotTime;
-    private long rechargeTime;
-    private Enemy target;
-    private int att;
-    private int def;
-    private float range;
-
-    public Turret(Vector2f pos) {
-        visible = false;
+    public Turret(String top, String bottom, boolean visible, int health, float angle, float rotationAngle, int rotationDelay, FloatRect dimensions,
+                   int scrapCost, int xpRequirement, int range, int AOESize, boolean shieldActive, int shieldTimer,
+                   String ID, String explosion, int width, int height, int row, int col, int delay) {
+        this.visible = visible;
         destroyed = false;
-        health = 0;
-        rotationSpeed = 0.0f;
-        angle = 0.0f;
-        speed = 0.0f;
-        top = null;
-        bottom = null;
-        position = new Vector2f(0.0f,0.0f);
-        target=null;
-        shotTime=0;
-        rechargeTime=1000;
-        att=10;
-        def=10;
-        range=200;
+        this.health = health;
+        this.rotationDelay = rotationDelay;
+        this.currentAngle = angle;
+        this.rotationAngle = rotationAngle;
+
+        float x = 0.2f * (dimensions.left + dimensions.width);
+        float y = 0.2f * (dimensions.top + dimensions.height);
+        float w = 0.8f * dimensions.width;
+        float h = 0.8f * dimensions.height;
+        this.top = new Button(top, new FloatRect(x, y, w, h), ID + " TOP", false);
+        this.top.setOriginCentre();
+        this.top.setAngleOfImage(angle);
+        this.top.setPositionOfImage(new Vector2f(dimensions.left + dimensions.width / 2, dimensions.top + dimensions.height / 2));
+
+        this.bottom = new Button(bottom, dimensions, ID + " BOTTOM", false);
+        this.scrapCost = scrapCost;
+        this.xpRequirement = xpRequirement;
+        this.range = range;
+        this.AOESize = AOESize;
+        this.shieldActive = shieldActive;
+        this.shieldTimer = shieldTimer;
+        this.enemyKillCount = 0;
+        this.ID = ID;
+        localElapsedTime = 0;
+        this.explosion = new Animation(explosion, width, height, row, col, delay, new Vector2f(dimensions.left, dimensions.top), 0, dimensions, false);
+        currentTime = 0;
+        prevTime = 0;
+        XPForEnemyKills = 0;
+        attackScore = 0;
+        XPMultiplier = 1;
     }
-
-    public void spawn(boolean visible, int health , Animation top, Animation bottom, Vector2f position){
-        top = new Animation("src/assets/Tiles.jpg",32,32,5,2,1000,position,3);
-
-        // TODO: 08/02/2016 Change this to a static image rather than an animation?
-        bottom = new Animation("src/assets/Tiles.jpg",32,32,5,2,1000,position,3);
-    }
-
+    
     @Override
     public void update() {
+        prevTime = currentTime;
+        currentTime = Window.getInstance().getElapsedTime();
+
+        localElapsedTime += currentTime - prevTime;
+
         if(destroyed){
-            //TODO 9/2/16 add explosion
-            System.out.println("Explosion animation to add later.");
-            //explosion.update();
+            explosion.update();
         }
         else if(visible){
+            if( localElapsedTime >= rotationDelay ){
+                localElapsedTime = 0;
+                currentAngle += rotationAngle;
+                top.setAngleOfImage(currentAngle);
+
+                if(currentAngle >= (360.0f * 3.14 / 180)){
+                    currentAngle = 0.0f;
+                }
+            }
+
             top.update();
             bottom.update();
         }
 
         if(health <= 0) {
             health = 0;
-            destroy();
+            visible = false;
+            destroyed = true;
         }
     }
 
     @Override
     public void render() {
         if(visible){
-            top.render();
             bottom.render();
+            top.render();
+
         }
         else if(destroyed){
-            System.out.println("Boom!");
-            //explosion.render();
+            explosion.render();
         }
+    }
+
+    public void resetAttackScore(){
+        attackScore = 0;
+    }
+
+    public void resetXP(){
+        XPForEnemyKills = 0;
+    }
+
+    public long getAttackScore(){
+        return attackScore;
+    }
+
+    //TODO: XP multiplier needs to be implemented properly based on scrap invested into research
+    public long getXPForEnemyKills(){
+        return XPForEnemyKills * XPMultiplier;
+    }
+
+    public void setXPMultiplier(int value){
+        XPMultiplier = value;
+    }
+
+    public String getID(){
+        return ID;
+    }
+
+    public int getEnemyKillCount(){
+        return enemyKillCount;
+    }
+
+    public void setShieldTimer(int shieldTimer){
+        this.shieldTimer = shieldTimer;
+    }
+
+    public void setAOESize(int AOESize){
+        this.AOESize = AOESize;
+    }
+
+    public int getAOESize(){
+        return AOESize;
+    }
+
+    public void setRange(int range){
+        this.range = range;
+    }
+
+    public int getRange(){
+        return range;
+    }
+
+    public void setXPRequirement(int requirement){
+        xpRequirement = requirement;
+    }
+
+    public int getXPRequirement(){
+        return xpRequirement;
+    }
+
+    public void setScrapCost(int scrapCost){
+        this.scrapCost = scrapCost;
+    }
+
+    public int getScrapCost(){
+        return scrapCost;
+    }
+
+    public void setRotationDelay(int delay){
+        rotationDelay = delay;
+    }
+
+    public int getTurretHealth(){
+        return health;
     }
 
     public void takeDamage(int damageCount){
         health -= damageCount;
     }
 
-    public void destroy(){
-        visible = false;
-        destroyed = true;
-        position = new Vector2f(0.0f, 0.0f);
+    public void repairTurret(int healthBoost){
+        health += healthBoost;
     }
 
     public void toggleVisibility(){
-        if (visible){
-            visible = !visible;
-            System.out.println("Turret hidden");
-        }
-        // Print to say that turret is visible / not visible.
-        System.out.println("Turret visible: " + visible);
-
-    }
-
-    public Enemy getTarget() {
-        return target;
-    }
-
-    public void setTarget(Enemy target) {
-        this.target = target;
-
-    }
-
-    public Vector2f getPos(){
-        return position;
-    }
-    public float getRange(){
-        return range;
-    }
-
-    public int getAtt() {
-        return att;
+        visible = !visible;
     }
 }
